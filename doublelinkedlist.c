40c830ca69aefc85ffb79a3ca5bfc920bf4ab824
bhve0693 <bhve0693@colorado.edu>
2017-09-08 17:16:18 -0600

    Included Enums,Added Header Files, minor corrections

--- doublelinkedlist.c
+++ doublelinkedlist.c
@@ -9,36 +9,17 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<stdint.h>
-#define HEAD 0
-//typedef uint8_t STATUS 
-
-struct NODE{
-uint32_t element;
-struct NODE *prev;
-struct NODE *next;
-};
-uint32_t global_index = 0;
-/*
-enum STATUS{
-0,1,
-};*/
-/*
+#include"doublelinkedlist.h"
 
 
-uint8_t destroy(struct NODE **hdnode_ptr);
-uint8_t add_node(struct NODE **hdnode_ptr, uint32_t data, uint32_t index);
-uint8_t remove_node(struct NODE **bsnode_ptr, uint32_t data, uint32_t index);
-uint8_t search(struct NODE **node_ptr, uint32_t data, uint32_t *index);
-uint32_t size(struct NODE **hdnode_ptr);
-*/
-uint8_t destroy(struct NODE **hdnode_ptr)
+enum Err_code destroy(struct NODE **hdnode_ptr)
 {
  
 struct NODE *temp = (struct NODE *)malloc(sizeof(struct NODE));
 
 
 if (!(*hdnode_ptr))
-    return 1; /*if no nodes are present */
+    return ERR_DLL_EMPTY; /*if no nodes are present */
 
 else 
     {
@@ -50,24 +31,25 @@ else
 	     free(temp);	
 	 }
 	 global_index = 0;
-	 return 0;
+	 return SUCCESS;
      }
 }
 
-uint8_t add_node(struct NODE **hdnode_ptr, uint32_t data, uint32_t index)
+enum Err_code add_node(struct NODE **hdnode_ptr, uint32_t data, uint32_t index)
 {
   struct NODE *new_node = (struct NODE *)malloc(sizeof(struct NODE));
   struct NODE *temp;
   //malloc fail Case 
   if(!new_node)
-     return 1;
+     return ERR_MALLOC_FAIL;
 
   new_node->element = data;
-  
-  
-  if(index > (global_index+1)) //Invalid Index
-     return 2;
 
+  if(index > (global_index)) //Invalid Index
+  {
+
+    return ERR_INVALID_INDEX;
+  }
 
     //No Head Case
     if(!(*hdnode_ptr))
@@ -76,7 +58,7 @@ uint8_t add_node(struct NODE **hdnode_ptr, uint32_t data, uint32_t index)
         new_node->prev = NULL;
         *hdnode_ptr = new_node;
         global_index++;
-        return 0;
+        return SUCCESS;
     }
   
     //Adding at Head Node
@@ -87,7 +69,7 @@ uint8_t add_node(struct NODE **hdnode_ptr, uint32_t data, uint32_t index)
         (*hdnode_ptr)->prev = new_node;
 	*hdnode_ptr = new_node;
         global_index++;
-        return 0;
+        return SUCCESS;
     }
     if(index == global_index) //Adding New Node at Tail
     {
@@ -100,7 +82,7 @@ uint8_t add_node(struct NODE **hdnode_ptr, uint32_t data, uint32_t index)
         new_node->next = NULL;
         temp->next = new_node;
         global_index++;
-        return 0; 
+        return SUCCESS; 
     }
     //Any general Node
     else
@@ -116,13 +98,13 @@ uint8_t add_node(struct NODE **hdnode_ptr, uint32_t data, uint32_t index)
         temp->prev = new_node;
 	new_node->prev->next = new_node;
         global_index++;
-        return 0;
+        return SUCCESS;
      }
   // Valid Index
 }//Function
 
 
-uint8_t remove_node(struct NODE **hdnode_ptr, uint32_t index)
+enum Err_code remove_node(struct NODE **hdnode_ptr, uint32_t index)
 {
   struct NODE *new_node = (struct NODE *)malloc(sizeof(struct NODE));
   struct NODE *temp,*temp_remove;
@@ -130,12 +112,12 @@ uint8_t remove_node(struct NODE **hdnode_ptr, uint32_t index)
       //No Head Case
     if(!(*hdnode_ptr))
     {
-        return 1;
+        return ERR_DLL_EMPTY;
     }
   
   
   if(index > (global_index)) //Invalid Index
-     return 2;
+     return ERR_INVALID_INDEX;
 
  
  
@@ -147,7 +129,7 @@ uint8_t remove_node(struct NODE **hdnode_ptr, uint32_t index)
         (*hdnode_ptr)->next = NULL;
 	*hdnode_ptr = temp;
         global_index--;
-        return 0;
+        return SUCCESS;
     }
     if(index == global_index) //Removing Node at Tail
     {
@@ -161,7 +143,7 @@ uint8_t remove_node(struct NODE **hdnode_ptr, uint32_t index)
 	temp = NULL;
 	free(temp);
         global_index--;
-        return 0; 
+        return SUCCESS; 
     }
     //Removing Any general Node
     else
@@ -179,17 +161,17 @@ uint8_t remove_node(struct NODE **hdnode_ptr, uint32_t index)
 	temp->next = NULL;
 	free(temp);
         global_index--;
-        return 0;
+        return SUCCESS;
      }
   // Valid Index
 }//Function
 
-uint8_t search(struct NODE **node_ptr, uint32_t data, uint32_t *index)
+enum Err_code search(struct NODE **node_ptr, uint32_t data, uint32_t *index)
 {
   uint32_t temp_index =0;
   if(!(*node_ptr))
   {
-    return 2;
+    return ERR_DLL_EMPTY;
   }
   struct NODE *temp;
   temp = *node_ptr;
@@ -199,14 +181,14 @@ uint8_t search(struct NODE **node_ptr, uint32_t data, uint32_t *index)
     temp= temp->next;
     if(!temp)
     {
-       return 1;
+       return ERR_DATA_NOT_FOUND;
     } 
     
     temp_index++;	
   }
   printf("Data %d is found at index %d \r\n",data,temp_index);
   *index = temp_index;
-  return 0;
+  return SUCCESS;
 }
 uint32_t size(struct NODE **hdnode_ptr)
 {
@@ -242,9 +224,7 @@ void dll_menu()
    uint32_t index;
    uint32_t ch;
    struct NODE *head = NULL;
-   uint8_t Err_code =0;
-   uint8_t status = 0;
-   //enum Errorcode Errorcode;
+   enum Err_code status;
     printf("Double Linked List Menu\n1.Add New Node\n2.Remove Node\n3.Search a node\n4.Size\n5.Destroy LL\n6.Exit\n Enter Your Choice(1-6)");
 	scanf("%u",&ch);
         while(ch!=6)
@@ -255,32 +235,32 @@ void dll_menu()
 				scanf("%u",&nodeelement);
 				printf("\nEnter the Index of new node to be added : ");
 				scanf("%u",&index);
-				Err_code = add_node(&head,nodeelement,index);
-				if(Err_code == 2) 
+				status = add_node(&head,nodeelement,index);
+				if(status == ERR_INVALID_INDEX) 
 				{
 					printf("Invalid Index !\n");
 					break;
 				}
-				if(Err_code == 0)
+				if(!status)
 				{
 					display(head);
 					break;
 				}
 			case 2: printf("\nEnter the Index of node to be removed : ");
 				scanf("%u",&index);
-				Err_code = remove_node(&head,index); 
-				if(Err_code == 2) 
+				status = remove_node(&head,index); 
+				if(status == ERR_INVALID_INDEX) 
 				{
 					printf("Invalid Index !\n");
 					break;
 				}
-				if(Err_code == 1)
+				if(status == ERR_DLL_EMPTY)
 				{
 					printf("No LL to remove node from\n");
 					break;
 				}
 
-				if(Err_code == 0)
+				if(!status)
 				{
 					display(head);
 					break;
@@ -289,12 +269,12 @@ void dll_menu()
 			case 3: printf("\nEnter data to be searched : ");
 				scanf("%u",&nodeelement);
 				status = search(&head,nodeelement,&index);
-				if(status == 2)
+				if(status == ERR_DLL_EMPTY)
 				{
 					printf("Empty DLL\n");
 					break;
 				}
-                                if(status == 1)
+                                if(status == ERR_DATA_NOT_FOUND)
                                 {
                                    printf("Data not Found!\n");
 					break;
@@ -303,16 +283,16 @@ void dll_menu()
 				break;
 
 			case 4: node_size = size(&head);
-				printf("\nSize of List:%u\n",node_size);
+				printf("\nSize of DLL:%u\n",node_size);
 				break;
 
-			case 5: Err_code = destroy(&head);
-				if(Err_code == 1)
+			case 5: status = destroy(&head);
+				if(status == ERR_DLL_EMPTY)
 				{
 					printf("\nDLL not available to destroy\n");
 					break;
 				}
-				if(Err_code == 0)
+				else
 				{
 					printf("\nDLL destroyed\n");
 					break;
@@ -331,64 +311,5 @@ int main()
 
   dll_menu();
   return 0;
-/*Test Code */
-/*
-status = add_node(&head,15,0); 
-status = add_node(&head,16,1); 
-status = add_node(&head,17,1); 
-status = add_node(&head,18,3); 
-status = add_node(&head,20,2); 
-status = add_node(&head,14,0);
-status = add_node(&head,13,0);
-status = add_node(&head,12,0);
-status = add_node(&head,11,0);
-status = add_node(&head,10,0);
-status = add_node(&head,0,1);
-status = add_node(&head,0,1);
-status = add_node(&head,0,1);
-status = add_node(&head,0,1);
-status = add_node(&head,0,1);
-status = add_node(&head,14,global_index);
-status = add_node(&head,13,global_index);
-status = add_node(&head,12,global_index);
-status = add_node(&head,11,global_index);
-status = add_node(&head,10,global_index);
-status = add_node(&head,0,global_index/2);
-status = add_node(&head,1,global_index/2);
-status = add_node(&head,2,global_index/2);
-status = add_node(&head,3,global_index/2);
-status = add_node(&head,4,global_index/2);
-status = remove_node(&head,0);
-status = remove_node(&head,0);
-status = remove_node(&head,0);
-status = remove_node(&head,0);
-status = remove_node(&head,0);
-status = remove_node(&head,0);
-status = remove_node(&head,global_index);
-status = remove_node(&head,global_index);
-status = remove_node(&head,global_index);
-status = remove_node(&head,global_index);
-status = remove_node(&head,global_index/2);
-status = remove_node(&head,global_index/2);
-status = remove_node(&head,global_index/2);
-temp = head; 
-while(temp)
-{
- printf("Data in Index %d is %d \r\n",ind,temp->element);
- ind++;
- temp =temp->next; 
-}
-
-search(&head,14,&search_index);
-search(&head,11,&search_index);
-search(&head,17,&search_index);
-printf("Search index value in main is %d\r\n",search_index);
-node_size = size(&head);
-printf("Size of the DLL is %d \r\n",node_size);
-printf("Global Size %d \r\n", global_index);
-destroy(&head);
-status = remove_node(&head,2);
-printf("Status in remove node is %d \r\n",status);
-*/
 }
 
